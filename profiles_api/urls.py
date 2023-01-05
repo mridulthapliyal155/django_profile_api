c6e8e54ce1c3788dae5157d60118e2b6c94fc093
mridul <thapliyamridul19@gmail.com>
2023-01-05 17:44:20 +0530

    initial commit

---
+++ profiles_api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from profiles_api import views
+
+router = DefaultRouter()
+router.register('profile',views.UserProfileViewSet)
+
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
+    path('',include(router.urls))
+]
